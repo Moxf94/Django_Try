032521d5692efe0c74a80ff4d9643144331cdb02
AIT <moxf94@gmail.com>
2024-02-01 17:48:38 +0700

    I'm back! Make some edits in main page, add some buttons, make dynamic numbers for page

--- try/news/urls.py
+++ try/news/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news-detail')
 ]
